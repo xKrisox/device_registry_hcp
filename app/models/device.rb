afefc641bf64f8865a2d9007323a4446945b81f9
Krzysztof Strzelecki <s185511@student.pg.edu.pl>
2025-07-13 17:56:40 +0200

    remove acts_as_paranoid_

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 # The Device model represents a physical device registered in the system.
 # Each device belongs to a user and must have a unique serial number.
 class Device < ApplicationRecord
-    acts_as_paranoid # Enables soft deletion for the Device model
+    #acts_as_paranoid # Enables soft deletion for the Device model
     belongs_to :user
     validates :serial_number, uniqueness: { conditions: -> { where(deleted_at: nil) } }
     validates :user_id, presence: true
