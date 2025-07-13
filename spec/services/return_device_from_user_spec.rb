4c23cd333f66e3213d8bb5fb12b9bbd2ca539150
Krzysztof Strzelecki <s185511@student.pg.edu.pl>
2025-07-13 07:30:23 +0200

    Add tests for ReturnDeviceFromUser service: device return logic and edge cases

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -4,4 +4,26 @@ require 'rails_helper'
 
 RSpec.describe ReturnDeviceFromUser do
   # TODO: Implement the tests for ReturnDeviceFromUser
+  describe '#call' do
+    let(:user) { create(:user) }
+    let(:serial_number) { 'ABC123' }
+    let!(:device) { Device.create!(serial_number: serial_number, user_id: user.id, returned: false) }
+
+    it 'marks the device as returned' do
+      described_class.new(user: user, serial_number: serial_number, from_user: user.id).call
+      expect(device.reload.returned).to eq(true)
+    end
+
+    it 'does not mark device as returned if already returned' do
+      device.update!(returned: true)
+      described_class.new(user: user, serial_number: serial_number, from_user: user.id).call
+      expect(device.reload.returned).to eq(true)
+    end
+
+    it 'does nothing if device does not belong to user' do
+      other_user = create(:user)
+      described_class.new(user: other_user, serial_number: serial_number, from_user: other_user.id).call
+      expect(device.reload.returned).to eq(false)
+    end
+  end
 end
