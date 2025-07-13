d4dbd30cc9470ce648e50563436b045dd24f31a9
Krzysztof Strzelecki <s185511@student.pg.edu.pl>
2025-07-13 17:08:41 +0200

    Finalize AssignDeviceToUser service: enforce business rules and error handling

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -4,7 +4,7 @@ class AssignDeviceToUser
   def initialize(requesting_user:, serial_number:, new_device_owner_id:)
     @requesting_user = requesting_user
     @serial_number = serial_number
-    @new_device_owner_id = new_device_owner_id
+    @new_device_owner_id = new_device_owner_id.to_i
   end
 
   def call
@@ -12,15 +12,20 @@ class AssignDeviceToUser
       raise RegistrationError::Unauthorized
     end
 
-    device = Device.find_by(serial_number: @serial_number)
+    device = Device.where(serial_number: @serial_number, deleted_at: nil).order(created_at: :desc).first
     if device
-      if device.user_id == @new_device_owner_id
+      if device.user_id == @new_device_owner_id && !device.returned
         raise AssigningError::AlreadyUsedOnUser
-      else
+      elsif device.user_id != @new_device_owner_id && !device.returned
         raise AssigningError::AlreadyUsedOnOtherUser
       end
     end
 
-    Device.create!(serial_number: @serial_number, user_id: @new_device_owner_id)
+    returned_device = Device.where(serial_number: @serial_number, user_id: @new_device_owner_id, returned: true, deleted_at: nil).exists?
+    if returned_device
+      raise AssigningError::AlreadyUsedOnUser
+    end
+
+    Device.create!(serial_number: @serial_number, user_id: @new_device_owner_id, returned: false)
   end
 end
