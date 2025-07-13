3cf743ac58bba5d05b529f5545308ea81b6e3322
Krzysztof Strzelecki <s185511@student.pg.edu.pl>
2025-07-13 16:48:04 +0200

    Implement unassign action in DevicesController with comments and success response

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,21 +3,39 @@
 class DevicesController < ApplicationController
   before_action :authenticate_user!, only: %i[assign unassign]
   def assign
-    AssignDeviceToUser.new(
+
+    result = AssignDeviceToUser.new(
       requesting_user: @current_user,
-      serial_number: params[:serial_number],
-      new_device_owner_id: params[:new_device_owner_id]
+      serial_number: device_params[:serial_number],
+      new_device_owner_id: device_params[:new_owner_id].to_i
     ).call
-    head :ok
+    #head :ok
+    render json: { success: true }, status: :ok
+    
+  rescue RegistrationError::Unauthorized
+    render json: { error: 'Unauthorized' }, status: :unprocessable_entity
+  rescue AssigningError::AlreadyUsedOnUser
+    render json: { error: 'Device already assigned to this user' }, status: :unprocessable_entity
+  rescue AssigningError::AlreadyUsedOnOtherUser
+    render json: { error: 'Device already assigned to another user' }, status: :unprocessable_entity
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def unassign
     # TODO: implement the unassign action
+    ReturnDeviceFromUser.new(
+      user: @current_user,
+      serial_number: device_params[:serial_number],
+      from_user: @current_user.id
+    ).call
+    render json: { success: true }, status: :ok
+    
   end
 
   private
 
   def device_params
-    params.permit(:new_owner_id, :serial_number)
+    params.require(:device).permit(:serial_number).merge(new_owner_id: params[:new_owner_id].to_i)
   end
 end
